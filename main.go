be2ad92a15b773b68020908f104d6e31303d8b0d
prisciladuarte <duartecostap@gmail.com>
2022-02-21 21:30:16 -0300

    Primeira versao da estrutura

--- main.go
+++ main.go
@@ -5,9 +5,14 @@ import "fmt"
 func main() {
 	var nome string
 	var numberTicket float32
+	var multiplica float32
+	multiplica = numberTicket * 10.5
+
 	fmt.Println("Qual é o seu nome ?")
 	fmt.Scanln(&nome)
 	fmt.Print("Quantos tickets você quer reservar? \n")
 	fmt.Scanf("%f\n", &numberTicket)
-	fmt.Printf(" deseja mesmo reservar %f", numberTicket)
+	fmt.Printf("Deseja mesmo reservar %f ?\n", numberTicket)
+
+	fmt.Println(nome, ", você reservou:", numberTicket, "ticket(s), no valor de R$ ", multiplica)
 }
